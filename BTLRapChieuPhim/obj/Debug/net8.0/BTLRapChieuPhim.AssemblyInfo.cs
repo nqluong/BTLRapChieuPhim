888de7604762eeb14342f01cd79e3f1324320a4d
Levanchung2502 <chungne25022004@gmail.com>
2024-10-23 13:39:27 +0700

    update layout

--- BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
+++ BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4c945e6b4341970ffd4803d83702a6d72507fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ae1ad4a64d6716d415a9892120b95298ff5eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
