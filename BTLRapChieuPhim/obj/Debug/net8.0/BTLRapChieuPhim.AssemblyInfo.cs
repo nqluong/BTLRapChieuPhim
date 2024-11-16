788118ff73fac1047acacc11528f76248781d57b
nqluong <nguyenluong052004@gmail.com>
2024-11-17 02:01:21 +0700

    update .gitinore

--- BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
+++ BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2228b4f3a27be365deddb9f272e2621d48a189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a978e086f9e966eada5dd172a1acb035c32b34c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
