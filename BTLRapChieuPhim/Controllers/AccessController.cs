fc086d641ffbd21e75cd6074c311e470fcdc07d2
luu-dat <luudat148@gmail.com>
2024-11-16 22:09:27 +0700

    Đăng nhập / Đăng Xuất (lỗi chưa chạy được)
    Thêm phần đăng nhập đăng xuất cho admin
    + Khi đăng nhập nếu tài khoản có MaTK nằm trong 1 - 6 thì sẽ chuyển đến trang _LayoutTemplateAdmin thay vì _LayoutTemplate (tức giao diện người dùng)
    + Ở LayoutTemplateAdmin chỉnh chức năng đăng xuất sao cho khi ấn vào thì chuyển đến trang Login

--- BTLRapChieuPhim/Controllers/AccessController.cs
+++ BTLRapChieuPhim/Controllers/AccessController.cs
@@ -21,13 +21,21 @@ namespace BTLRapChieuPhim.Controllers
         [HttpPost]
         public IActionResult Login(TaiKhoan user)
         {
-            if(HttpContext.Session.GetString("UserName")==null)
+            if (HttpContext.Session.GetString("User Name") == null)
             {
                 var u = db.TaiKhoans.Where(x => x.Username.Equals(user.Username) && x.Password.Equals(user.Password)).FirstOrDefault();
                 if (u != null)
                 {
-                    HttpContext.Session.SetString("UserName", u.Username.ToString());
-                    return RedirectToAction("Index", "Home");
+                    HttpContext.Session.SetString("User Name", u.Username.ToString());
+                    // Kiểm tra MaTk
+                    if (u.MaTk >= 1 && u.MaTk <= 6)
+                    {
+                        return RedirectToAction("Index", "HomeAdmin"); // Chuyển đến trang admin
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home"); // Chuyển đến trang người dùng
+                    }
                 }
             }
             return View();
--- BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
+++ BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b8787d986fc1cacc57efd86b05dda034009efb0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87feb6b51cb4ef0ef337183d5ee0ba6ba63e50d2")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4416a8dc5e4c795409e5943ac497a68f0c392073")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
