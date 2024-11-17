49238041e316c2c9b65fad506fdb38d4529ad9cc
nqluong <nguyenluong052004@gmail.com>
2024-11-17 14:31:19 +0700

    update luong

--- BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
+++ BTLRapChieuPhim/obj/Debug/net8.0/BTLRapChieuPhim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2228b4f3a27be365deddb9f272e2621d48a189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9d41607780dcbd0c0c4bcca80d3400809c1d03")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLRapChieuPhim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
